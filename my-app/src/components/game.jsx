dbe4d1e70cf8673d15a61b2ea5201991a1072a01
mayabridgewater <55682563+mayabridgewater@users.noreply.github.com>
2020-01-23 13:45:24 +0200

    generate guess/ upload to database

--- my-app/src/components/game.jsx
+++ my-app/src/components/game.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Cookies from 'js-cookie';
 
-import {getRange} from '../api/getData';
+import {getRange, getGuesses, uploadeGuesses} from '../api/getData';
 
 export default class Game extends React.Component {
     constructor() {
@@ -9,8 +9,11 @@ export default class Game extends React.Component {
         this.state = {
             from: '',
             to: '',
-            guess: ''
+            guess: '',
+            guessed: []
         }
+        this.generateGuess = this.generateGuess.bind(this);
+        this.setGuess = this.setGuess.bind(this)
     }
 
     async componentDidMount() {
@@ -18,15 +21,43 @@ export default class Game extends React.Component {
         let range = await getRange(player.id);
         const rangeArray = range[0].range.split('-');
         this.setState({
-            from: rangeArray[0],
-            to: rangeArray[1]
-        })
+            from: parseInt(rangeArray[0]),
+            to: parseInt(rangeArray[1])
+        }, this.generateGuess)
     }
+
+    async generateGuess() {
+        const guess =  Math.floor(Math.random() * (this.state.to - this.state.from + 1) ) + this.state.from;
+        const guesses = await getGuesses(JSON.parse(Cookies.get('player')).id);
+        if (!guesses[0].guesses) {
+            this.setState({
+                guessed: []
+            }, () => this.setGuess(guess))
+        }else {
+            this.setState({
+                guessed: guesses[0].guesses.split(',')
+            }, () => this.setGuess(guess))
+        }
+    }
+
+    async setGuess(guess) {
+       if (this.state.guessed.includes(guess)) {
+           window.location.replace('/game')
+       } else {
+           let guesses = this.state.guessed.splice();
+            guesses.push(guess);
+            const strGuesses = guesses.join(',');
+           this.setState({
+               guess: guess
+           })
+           const success = await uploadeGuesses({guess: strGuesses, id: JSON.parse(Cookies.get('player')).id})
+       }
+    }
+
     render() {
-        console.log(this.state)
         return (
             <div>
-                <h3>Computers Guess: </h3>
+                <h3>Computers Guess: {this.state.guess && this.state.guess}</h3>
             </div>
         )
     }
